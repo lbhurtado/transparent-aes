37e1381a9a0effc2b90c1555cd9b5249c7fcccd6
Lester Hurtado <lester@hurtado.ph>
2019-07-06 17:06:47 +0800

    apply results to ballot pivot

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,18 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Image;
+use Illuminate\Support\Arr;
 use LBHurtado\Ballot\Models\Ballot;
 use App\Http\Requests\BallotImageRequest;
+use LBHurtado\Ballot\Models\{Position, Candidate};
 
 class ImageController extends Controller
 {
     public function store(BallotImageRequest $request)
     {
-        $image = Image::persist($request)->transfuseQRCode()->processMarkings();
-        $ballot = Ballot::updateOrCreate(['code' => $image->qr_code], ['image' => $image->url]);
+        $image = $this->getImageFromRequest($request);
+        $ballot = $this->getBallotFromImage($image);
 
-        dd($image->getMarkings());
+//        dd($image->getMarkings());
+        foreach ($image->getMarkings() as $group_id => $candidate_code) {
+            if ($candidate = Candidate::where('code', $candidate_code)->first()){
+                $ar = explode(':', $group_id);
+                $position_code = Arr::get($ar, 0);
+                $position = Position::where('name', $position_code)->first();
+//                dd($position->seats);
+                $seat = Arr::get($ar, 1, 1);
+                if ($seat <= $position->seats)
+                    $ballot->updatePivot($candidate, $seat);
+            }
+        }
 
         return $ballot->with('positions')->get();
+    }
+
+    protected function getImageFromRequest(BallotImageRequest $request)
+    {
+        return Image::persist($request)->transfuseQRCode()->processMarkings();
+    }
+
+    protected function getBallotFromImage(Image $image)
+    {
+        return Ballot::updateOrCreate(['code' => $image->qr_code], ['image' => $image->url]);
     }
 }
